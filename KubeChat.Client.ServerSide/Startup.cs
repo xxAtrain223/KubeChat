9c6e3209a4b0cca090260a245f74b34dad44a59a
Anthony Stewart <anthony.l.stewart@outlook.com>
2021-01-02 19:39:16 -0600

    Renamed KubeChat.Client to KubeChat.Client.ServerSide, Updated ServerSide Dockerfile.

--- KubeChat.Client.ServerSide/Startup.cs
+++ KubeChat.Client.ServerSide/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace KubeChat.Client
+namespace KubeChat.Client.ServerSide
 {
     public class Startup
     {
