7adcf8cb497867f6e76a86350b187fe62c34acd9
Anthony Stewart <anthony.l.stewart@outlook.com>
2021-01-10 21:28:56 -0600

    Added AgonesSDK to the Server to enable the Health Ping.

--- KubeChat.Server/Startup.cs
+++ KubeChat.Server/Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Agones;
+using System.Threading;
+using Microsoft.Extensions.Logging;
 
 namespace KubeChat.Server
 {
@@ -25,11 +28,35 @@ namespace KubeChat.Server
         {
             services.AddSignalR();
             services.AddCors();
+
+            // Add a singleton service to communicate with Agones
+            services.AddSingleton((IServiceProvider serviceProvider) =>
+            {
+                var logger = serviceProvider.GetRequiredService<ILogger<Startup>>();
+                var agones = new AgonesSDK();
+
+                try
+                {
+                    var connected = agones.ConnectAsync().Result;
+                    if (!connected)
+                    {
+                        logger.LogCritical("Agones did not connect.");
+                    }
+                }
+                catch (Exception e)
+                {
+                    logger.LogCritical(e, "Agones did not connect with exception.");
+                }
+
+                return agones;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHostApplicationLifetime hostAppLifetime, ILogger<Startup> logger)
         {
+            logger.LogInformation($"KubernetesResource: {Configuration.GetValue<string>("KubernetesResource")}");
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -38,6 +65,44 @@ namespace KubeChat.Server
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+            }
+
+            // Only start the Agones health ping if running an Agones GameServer
+            if (Configuration.GetValue<string>("KubernetesResource") == "GameServer")
+            {
+                hostAppLifetime.ApplicationStarted.Register(async () =>
+                {
+                    var agones = app.ApplicationServices.GetRequiredService<AgonesSDK>();
+
+                    logger.LogDebug($"Agones is {(agones != null ? "not" : "")} null");
+
+                    // Alert Agones that this server is ready once the application has finished starting
+                    await agones.ReadyAsync();
+
+                    // Keep sending health pings until Shutdown
+                    var running = true;
+
+                    // Send health ping every third of a period, to allow for at least a second try.
+                    var healthPeriod = TimeSpan.FromSeconds((await agones.GetGameServerAsync())
+                        .Spec.Health.PeriodSeconds) / 3;
+
+                    // Setup a callback to watch for relevant changes
+                    agones.WatchGameServer((Agones.Dev.Sdk.GameServer gameServer) =>
+                    {
+                        running = gameServer.Status.State != "Shutdown";
+                        healthPeriod = TimeSpan.FromSeconds(gameServer.Spec.Health.PeriodSeconds) / 3;
+                    });
+
+                    // Start a thread to send health pings
+                    new Thread(async () =>
+                    {
+                        while (running)
+                        {
+                            await agones.HealthAsync();
+                            Thread.Sleep(healthPeriod);
+                        }
+                    }).Start();
+                });
             }
 
             app.UseCors(builder =>
